f3bb5c5f4356f3056ddb7c9a18e1f02f6ddc7304
mhendricks96 <mhendricks96@yahoo.com>
2022-03-29 17:50:29 -0700

    switched out get user model for auth user model

--- poop_profiles/models.py
+++ poop_profiles/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.conf import settings
 
 # Create your models here.
 
 class PoopProfile(models.Model):
   poopInfo = models.IntegerField()
   nickname = models.CharField(max_length=64)
-  user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+  user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
