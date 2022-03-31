fad7d7df7f93e5a72ca0c27f80a820276b027a10
mhendricks96 <mhendricks96@yahoo.com>
2022-03-31 16:05:15 -0700

    pushing to main for deployment

--- poop_garden_api/settings.py
+++ poop_garden_api/settings.py
@@ -92,10 +92,21 @@ WSGI_APPLICATION = 'poop_garden_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': env.str("DATABASE_ENGINE"),
+        'NAME': env.str("DATABASE_NAME"),
+        'USER': env.str("DATABASE_USER"),
+        'PASSWORD': env.str("DATABASE_PASSWORD"),
+        'HOST': env.str("DATABASE_HOST"),
+        'PORT': env.int("DATABASE_PORT"),
     }
 }
 
