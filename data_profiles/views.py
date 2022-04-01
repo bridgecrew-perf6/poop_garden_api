297226a1d3a2851ac4651ebcaa668dd235cb76ab
mhendricks96 <mhendricks96@yahoo.com>
2022-04-01 15:02:10 -0700

    added some jwt stuff for authentication

--- data_profiles/views.py
+++ data_profiles/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import PoopProfileSerializer, FartProfileSerializer
 from .models import PoopProfile, FartProfile
 from rest_framework import response, request
@@ -8,27 +8,27 @@ from .permissions import isOwnerOrReadOnly
 
 
 # gives us the ability to add items
-class PoopProfileList(generics.ListCreateAPIView):
+class PoopProfileList(ListCreateAPIView):
 
-  permission_classes = (isOwnerOrReadOnly,)
+  # permission_classes = (isOwnerOrReadOnly,)
   queryset = PoopProfile.objects.all()
   serializer_class = PoopProfileSerializer
 
 # gives us the ability to update and delete
-class PoopProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-  permission_classes = (isOwnerOrReadOnly,)
+class PoopProfileDetail(RetrieveUpdateDestroyAPIView):
+  # permission_classes = (isOwnerOrReadOnly,)
   queryset = PoopProfile.objects.all()
   serializer_class = PoopProfileSerializer
 
 
-class FartProfileList(generics.ListCreateAPIView):
-  permission_classes = (isOwnerOrReadOnly,)
+class FartProfileList(ListCreateAPIView):
+  # permission_classes = (isOwnerOrReadOnly,)
   queryset = FartProfile.objects.all()
   serializer_class = FartProfileSerializer
 
 # gives us the ability to update and delete
-class FartProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-  permission_classes = (isOwnerOrReadOnly,)
+class FartProfileDetail(RetrieveUpdateDestroyAPIView):
+  # permission_classes = (isOwnerOrReadOnly,)
   queryset = FartProfile.objects.all()
   serializer_class = FartProfileSerializer
 
