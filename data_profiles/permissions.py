f331b343fc5f209c29c22a947c007add4458fc5b
mhendricks96 <mhendricks96@yahoo.com>
2022-04-13 14:33:34 -0700

    made user create view open to be seen without being signed in

--- data_profiles/permissions.py
+++ data_profiles/permissions.py
@@ -11,3 +11,9 @@ class isOwnerOrReadOnly(permissions.BasePermission):
       return True
 
     return object.user == request.user
+
+class AllowAny(permissions.BasePermission):
+
+  def has_object_permission(self, request, view, object):
+    return True
+
--- data_profiles/views.py
+++ data_profiles/views.py
@@ -1,9 +1,11 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .serializers import PoopProfileSerializer, FartProfileSerializer, UserSerializer
+from email.mime import base
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
+from .serializers import PoopProfileSerializer, FartProfileSerializer, UserSerializer, CreateUserSerializer
 from .models import PoopProfile, FartProfile
 from rest_framework import response, request
 from rest_framework import status
-from .permissions import isOwnerOrReadOnly
+from .permissions import isOwnerOrReadOnly, AllowAny
+from rest_framework import permissions
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -12,13 +14,13 @@ from django.contrib.auth.models import User
 # gives us the ability to add items
 class PoopProfileList(ListCreateAPIView):
 
-  # permission_classes = (isOwnerOrReadOnly,)
+  # permission_classes = (isAuthenticated,)
   queryset = PoopProfile.objects.all()
   serializer_class = PoopProfileSerializer
 
 # gives us the ability to update and delete
 class PoopProfileDetail(RetrieveUpdateDestroyAPIView):
-  # permission_classes = (isOwnerOrReadOnly,)
+  # permission_classes = (isAuthenticated,)
   queryset = PoopProfile.objects.all()
   serializer_class = PoopProfileSerializer
 
@@ -43,4 +45,7 @@ class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class UserCreate(CreateAPIView):
+  permission_classes = (AllowAny,)
+  serializer_class = CreateUserSerializer
 
